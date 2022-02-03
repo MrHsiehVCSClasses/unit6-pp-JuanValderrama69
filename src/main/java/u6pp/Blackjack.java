6651a36776cf4f23ab4c03a9c94e4582ab2e9a0b
Juan Valderrama <90728114+JuanValderrama69@users.noreply.github.com>
2022-02-03 21:21:00 +0000

    Ghv

--- src/main/java/u6pp/Blackjack.java
+++ src/main/java/u6pp/Blackjack.java
@@ -1,2 +1,4 @@
 package u6pp;
 
+
+
--- src/main/java/u6pp/Deck.java
+++ src/main/java/u6pp/Deck.java
@@ -1,1 +1,46 @@
 package u6pp;
+
+public class Deck
+{
+    private Card[] deck;
+    private int cardleft = 51;
+
+
+  public Deck(){
+        deck = new Card[52];
+        int index = 0;
+        for(String Suit : Card.SUITS)
+        {
+            for(String Value : Card.VALUES)
+            {   
+              deck[index] = new Card(Suit, Value);
+              index+1; 
+              }
+            
+        }
+        index-1;  
+    }
+
+
+  public int numLeft()
+    { return this.cardleft + 1;
+  }  
+  public Card deal()
+    {
+  Card temp = deck[cardleft];
+        cardleft--;
+        return temp;  
+    }
+
+        public void shuffle()
+{
+for(int i = 0; i < deck.length; i++) {
+int index = (int)(Math.random() * 52);
+  Card currentCard = deck[index];
+  deck[index] = deck[i];
+ deck[i] = currentCard;
+cardleft = 51; }
+      }  
+
+    
+    }
